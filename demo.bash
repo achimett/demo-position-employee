24e8c0e7edc44d9ac7c8079889d434ce47e3a71c
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-06-25 15:40:56 +0200

    Corrected typo

--- demo.bash
+++ demo.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "=== PUSH some Employees ===";
+echo "=== POST some Employees ===";
 curl -i -H "Content-Type:application/json" -d '{"name": "Martha","surname":"Sutherland","birthDate":"2000-07-30","gender":"FEMALE"}' http://localhost:8080/employees
 curl -i -H "Content-Type:application/json" -d '{"name": "John","surname":"Doe","birthDate":"1990-10-10","gender":"MALE"}' http://localhost:8080/employees
 curl -i -H "Content-Type:application/json" -d '{"name": "Alice","surname":"Barnum","birthDate":"1993-01-01","gender":"FEMALE"}' http://localhost:8080/employees
@@ -20,7 +20,7 @@ echo "This should be done by searching the right resource URI with HATEOAS and t
 curl -i -X PATCH -H "Content-Type:application/json" -d '{"name": "Anna"}' http://localhost:8080/employees/3
 echo
 
-echo "=== PUSH some Positions ==="
+echo "=== POST some Positions ==="
 curl -i -H "Content-Type:application/json" -d '{"name":"Developer", "desc":"Writes software"}' http://localhost:8080/positions
 curl -i -H "Content-Type:application/json" -d '{"name":"ProjectManager", "desc":"Leads developers"}' http://localhost:8080/positions
 echo
